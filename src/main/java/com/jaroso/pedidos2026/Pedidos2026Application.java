e1444b71993eaccf985cb4bca1a77555a8f8e2ae
jjguillen <jjavierguillen@gmail.com>
2026-01-16 17:40:07 +0100

    Commit inicial

---
+++ src/main/java/com/jaroso/pedidos2026/Pedidos2026Application.java
@@ -0,0 +1,13 @@
+package com.jaroso.pedidos2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pedidos2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pedidos2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jaroso/pedidos2026/Pedidos2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaroso.pedidos2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pedidos2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
