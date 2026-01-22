d8490ec2661d6083431cbed8110c290799f4cbdb
jjguillen <jjavierguillen@gmail.com>
2026-01-22 20:32:34 +0100

    Final pedidos, y nuevos dtos para los clientesbasicos para no sacar en un pedido también los clientes

--- src/main/java/com/jaroso/pedidos2026/dtos/ClienteBasicoDto.java
+++ src/main/java/com/jaroso/pedidos2026/dtos/ClienteBasicoDto.java
@@ -3,5 +3,5 @@ package com.jaroso.pedidos2026.dtos;
 import java.util.List;
 
 //Nos falta poner los pedidos
-public record ClienteBasicoDto(Long id, String nombre, String email) {
+public record ClienteBasicoDto(Long id, String nombre) {
 }
